f091ec4470bddb4d5d5e81462b7e1cb467c38e6e
TKYK <konno.takeyuki@gmail.com>
2021-03-21 21:06:58 +0900

    マシン詳細に総合評価の表示を追加

--- v2/asset/js/results.js
+++ v2/asset/js/results.js
@@ -1,7 +1,4 @@
-﻿var typeValue = new Array("", "speed", "power", "corner", "stamina", "weight", "gearfuka", "powerloss", "speedloss", "aerodf", "setsuden", "seishin", "thrust", "tiremasatsu", "tiresenkai", "tirehanpatsu", "tirekei", "rollermasatsu", "rollerteikou", "wave", "offload", "gearrate", "ampere", "brake", "stabilizer", "digital", "taifuu", "taisui", "curvestamina", "jumpstamina", "tread", "wheelbase", "rollerwidth", "rollerbase");
-var typeView = new Array("", "スピード ", "パワー ", "コーナー安定 ", "スタミナ耐久 ", "重さ ", "ギヤ負荷 ", "パワーロス ", "スピードロス ", "ｴｱﾛﾀﾞｳﾝﾌｫｰｽ ", "節電 ", "制振 ", "スラスト角 ", "タイヤ摩擦 ", "タイヤ旋回 ", "タイヤ反発 ", "タイヤ径 ", "ローラー摩擦 ", "ローラー抵抗 ", "ウェーブ ", "オフロード ", "ギヤ比 ", "消費電流 ", "ブレーキ減速 ", "スタビ減速 ", "デジタル ", "耐風 ", "耐水 ", "カーブ耐久 ", "ジャンプ耐久 ", "トレッド ", "ﾎｲｰﾙﾍﾞｰｽ ", "ローラー幅 ", "ﾛｰﾗｰﾍﾞｰｽ ");
-
-function All_Calc() {
+﻿function All_Calc() {
 	window.parent.mains.All_Calc();
 }
 function UrlView(value1) {
@@ -9,8 +6,8 @@ function UrlView(value1) {
 }
 
 function View_Result() {
-	//var typeValue = window.parent.mains.typeValue;
-	//var typeView = window.parent.mains.typeView;
+	var typeValue = window.parent.mains.typeValue;
+	var typeView = window.parent.mains.typeView;
 	var writeValue = "<table class='cstable'><tr>";
 	for (var i = 1; i < typeValue.length; i++) {
 		if (i > 1 && (i - 1) % 5 == 0) {
@@ -29,3 +26,7 @@ function View_Result() {
 	writeValue += "</tr></table>";
 	document.getElementById("bmain").innerHTML = writeValue;
 }
+
+window.addEventListener("load", function() {
+	View_Result();
+});
