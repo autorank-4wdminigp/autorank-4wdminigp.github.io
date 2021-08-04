73e51101fd43f34d3802624a7cde85c4ae5127e1
autorank-4wdminigp <62088886+autorank-4wdminigp@users.noreply.github.com>
2021-08-04 15:36:31 +0900

    Add files via upload

--- asset/js/mainscommon.js
+++ asset/js/mainscommon.js
@@ -395,13 +395,16 @@ function Diagnosis_Calc(resultValueKai, shindantire, shindantirekei) {
 	var bodyStamina = 1.0;
 	if (bodyOption1 == 5) bodyStamina += 0.1;
 	if (bodyOption1 == 15) bodyStamina += 0.2;
+	if (bodyOption1 == 45) bodyStamina += 0.25;
 	if (bodyOption1 == 25) bodyStamina += 0.2;
 	if (bodyOption1 == 35) bodyStamina += 0.2;
 	if (bodyOption1 == 52) bodyStamina += 0.1;
 	if (bodyOption2 == 5) bodyStamina += 0.05;
 	if (bodyOption2 == 15) bodyStamina += 0.1;
+	if (bodyOption2 == 45) bodyStamina += 0.15;
 	if (bodyOption3 == 5) bodyStamina += 0.05;
 	if (bodyOption3 == 15) bodyStamina += 0.1;
+	if (bodyOption3 == 45) bodyStamina += 0.15;
 	var staminaValue = (resultValueKai[4] + resultValueKai[28]) * bodyStamina;
 	diagnosis[diagnosisValue[24]] = staminaValue;
 	diagnosis[diagnosisValue[25]] = staminaValue * staminaValue * 0.5975 / (speedValue2 * speedValue2 * weightValue * 0.42); //0.4825 0.5975 0.7125
