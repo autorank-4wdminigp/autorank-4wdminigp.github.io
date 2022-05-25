8c519e7ae1b607f11636e82e6ea1f3609c2f9b6f
autorank-4wdminigp <62088886+autorank-4wdminigp@users.noreply.github.com>
2022-05-25 16:27:30 +0900

    Add files via upload

--- v2/asset/js/menus.js
+++ v2/asset/js/menus.js
@@ -6,17 +6,18 @@ function View_Menus() {
 	menuValue[1] = new Array(34, -1, 3, -1);
 	menuValue[2] = new Array(4, 6, 5, 7);
 	menuValue[3] = new Array(-1, 8, 9, 10);
-	menuValue[4] = new Array(-1, 14, 15, 16);
-	menuValue[5] = new Array(-1, 11, 12, 13);
-	menuValue[6] = new Array(-1, -1, -1, 17);
-	menuValue[7] = new Array(-1, -1, 19, 18);
-	menuValue[8] = new Array(-1, 20, 21, 22);
-	menuValue[9] = new Array(-1, 23, -1, 24);
-	menuValue[10] = new Array(25, 26, 27, 28);
-	menuValue[11] = new Array(29, 30, 31, 32);
-	menuValue[12] = new Array(-1, -1, 33, -1);
+	menuValue[4] = new Array(-1, 35, -1, -1);
+	menuValue[5] = new Array(-1, 14, 15, 16);
+	menuValue[6] = new Array(-1, 11, 12, 13);
+	menuValue[7] = new Array(-1, -1, -1, 17);
+	menuValue[8] = new Array(-1, -1, 19, 18);
+	menuValue[9] = new Array(-1, 20, 21, 22);
+	menuValue[10] = new Array(-1, 23, -1, 24);
+	menuValue[11] = new Array(25, 26, 27, 28);
+	menuValue[12] = new Array(29, 30, 31, 32);
+	menuValue[13] = new Array(-1, -1, 33, -1);
 	var writeValue = "<table class='csmenutable'>";
-	for (var j = 0; j <= 12; j++) {
+	for (var j = 0; j <= 13; j++) {
 		writeValue += "<tr>";
 		for (var i = 0; i < 4; i++) {
 			if (menuValue[j][i] != -1) {
